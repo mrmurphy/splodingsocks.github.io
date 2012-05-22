02234c700221d1f8e550de4568266376df723292
Murphy <murphyspublic@gmail.com>
2012-05-22 15:19:40 -0600

    Fixed the menu for display in Firefox. Fixed some colors and the blog link.

---
+++ source/javascripts/search.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+  var Swiftype = window.Swiftype || {};
+  (function() {
+    Swiftype.key = 'ypQ2wDEsVAsqUvGhWpiN';
+    Swiftype.inputElement = '#main-search-box';
+    Swiftype.resultContainingElement = '#st-results-container';
+    Swiftype.attachElement = '#main-search-box';
+    Swiftype.renderStyle = "new_page";
+    Swiftype.resultPageURL = alert('Replace this with your desired result URL.');
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = "//swiftype.com/embed.js";
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+  }());
+</script>
