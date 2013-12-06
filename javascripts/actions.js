584a8eab147d71edd195cb97eafad0a376ace967
Murphy Randle <murphyspublic@gmail.com>
2013-12-05 22:13:12 -0700

    Resetting to plain old Jekyll

--- javascripts/actions.js
+++
@@ -1,9 +0,0 @@
-jQuery.noConflict();
-jQuery(document).ready(function($) {
-    $('form').inputLabels();
-    var searchwrapper = $('#search-wrapper').hide()
-    $('#search-button-nav').click(function(e) {
-      e.preventDefault();
-      searchwrapper.slideToggle();
-    });
-}); 
